1908041abea40aec168b045a6514f93c84099708
Your Name <you@example.com>
2026-03-30 09:57:06 -0300

    fix: using cmake and conflict merge

--- examples/KnobShowcase/Source/Main.cpp
+++ examples/KnobShowcase/Source/Main.cpp
@@ -1,4 +1,9 @@
-#include <JuceHeader.h>
+#if __has_include(<JuceHeader.h>)
+    #include <JuceHeader.h>                          // Projucer build
+#else
+    #include <juce_gui_basics/juce_gui_basics.h>     // CMake build
+    #include <juce_gui_extra/juce_gui_extra.h>
+#endif
 
 #include "ShowcaseComponent.h"
 
